f3b443b7caf593c166b256546ef10a604c657770
toddw <toddw440@gmail.com>
2026-06-11 22:22:14 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+/* Offline cache: network-first so updates land, cached copy when out of signal. */
+var CACHE = 'truck-parts-v1';
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) {
+    return c.addAll(['./', './index.html', './manifest.json', './icon.svg']);
+  }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(caches.keys().then(function (keys) {
+    return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+  }).then(function () { return self.clients.claim(); }));
+});
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(function (resp) {
+      var copy = resp.clone();
+      caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+      return resp;
+    }).catch(function () {
+      return caches.match(e.request, { ignoreSearch: true });
+    })
+  );
+});
